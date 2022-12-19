3f2fadd74e739ffb98ddc49afd5ae5bf66ccb1d0
amanprajapat08278 <amanprajapat08278@gmail.com>
2022-12-19 15:04:53 +0530

    hfiah

---
+++ src/controllers/userController.js
@@ -0,0 +1,20 @@
+
+
+const createUser = async (req, res)=>{
+
+}
+
+const loginUser = async (req, res)=>{
+    
+}
+
+const getUser = async (req, res)=>{
+    
+}
+
+
+const updateUser = async (req, res)=>{
+    
+}
+
+module.exports = {createUser, loginUser, getUser, updateUser}
